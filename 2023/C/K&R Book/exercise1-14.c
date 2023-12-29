e7ed256837348f0a77aefc77f204fa72b126df42
Radu <radu.rentea@gmail.com>
2023-12-29 12:12:39 +0200

    Finish solution

--- 2023/C/K&R Book/exercise1-14.c
+++ 2023/C/K&R Book/exercise1-14.c
@@ -5,32 +5,29 @@ in its input. */
 #define MAX_CHARACTERS 100
 #define SPACE '-'
 
-int main(void)
-{
+int main(void) {
     int c;
     int arrChars[MAX_CHARACTERS];
     int arrCharsCount[MAX_CHARACTERS];
     int flagChar = 0;
     int count = 0;
+    int maxUsedChar = 0;
 
-    while((c = getchar()) != EOF)
-    {
-        if (c != ' ' && c != '\t' && c != '\n')
-        {
+    while((c = getchar()) != EOF) {
+        if (c != ' ' && c != '\t' && c != '\n') {
             int i = 0;
-            for(i = 0; i < MAX_CHARACTERS; i++)
-            {
-                if(c == arrChars[i])
-                {
+            for(i = 0; i < MAX_CHARACTERS; i++) {
+                if(c == arrChars[i]) {
                     ++arrCharsCount[i];
+                    if(arrCharsCount[i] > maxUsedChar){
+                        maxUsedChar = arrCharsCount[i];
+                    }
                     flagChar = 1;
                     break;
                 }
             }
-            if(flagChar == 0)
-            {
-                if(i == MAX_CHARACTERS)
-                {
+            if(flagChar == 0) {
+                if(i == MAX_CHARACTERS) {
                     arrChars[count] = c;
                     arrCharsCount[count] = 1;
                     count++;
@@ -42,18 +39,28 @@ int main(void)
             }
             flagChar = 0;
         }
-            printf("count is: %d\n", count);
-        for(int i = 0; i < count; i++)
-        {
-            printf("asta-i elementul[%d]: %c ",i, arrChars[i]);
-            printf("\n");
-        }
-        for(int i = 0; i < count; i++)
-        {
-            printf("asta e numarul elementului [%d]: %d ",i, arrCharsCount[i]);
-            printf("\n");
-        }
+    }
+
+    printf("Max: %d\n", maxUsedChar);
+    for(int j = maxUsedChar; j > 0; j--) {
+        for(int i = 0; i < count; i++) {
+            if(j == arrCharsCount[i]) {
+                printf("_ ");
+                --arrCharsCount[i];
+            } else {
+                printf("  ");
             }
+        }
+        printf("\n");
+    }
+    
+    printf("\n");
+
+    for(int i = 0; i < count; i++) {
+        printf("%c ", arrChars[i]);
+    }
+
+    printf("\n");
         
     return 0;
 }
